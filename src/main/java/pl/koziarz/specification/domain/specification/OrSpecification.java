0ccfefdaaef642d0ba7ff360b8461e6a40f7e6e2
nadermedhet148 <nadermedhet142@gmail.com>
2021-08-28 19:32:00 +0200

    [FEAT] Composite specifications

---
+++ src/main/java/pl/koziarz/specification/domain/specification/OrSpecification.java
@@ -0,0 +1,34 @@
+package pl.koziarz.specification.domain.specification;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+public class AndSpecification<T> extends AbstractSpecification<T> {
+	
+	private Specification<T> first;
+	private Specification<T> second;
+	
+	public AndSpecification(Specification<T> first, Specification<T> second) {
+		this.first = first;
+		this.second = second;
+	}
+	
+	@Override
+	public boolean isSatisfiedBy(T t) {
+		return first.isSatisfiedBy(t) && second.isSatisfiedBy(t);
+	}
+
+	@Override
+	public Predicate toPredicate(Root<T> root, CriteriaBuilder cb) {
+		return cb.and(
+			first.toPredicate(root, cb), 
+			second.toPredicate(root, cb)
+		);
+	}
+	
+	@Override
+	public Class<T> getType() {
+		return first.getType();
+	}
+}
